9a9e7671a2186c6d74419668ebd0bec5f58106a5
Iluvatar42 <bt@langdalen.org>
2026-02-15 15:13:25 +0100

    Delete IluUltrawide directory

--- IluUltrawide/Source/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace IluUltrawide
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID    = "com.iluvatar.tailorsimulator.ultrawide";
-        public const string PLUGIN_NAME    = "IluUltrawide";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
